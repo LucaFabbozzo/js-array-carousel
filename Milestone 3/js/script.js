4a400e8714370551dfd77f9acbe63956c8270390
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-17 09:59:28 -0500

    correction into html structure

---
+++ Milestone 3/js/script.js
@@ -0,0 +1,41 @@
+// **MILESTONE 3**
+// Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+// **BONUS 1:**
+// Aggiungere il **ciclo infinito** del carosello. Ovvero se è attiva la prima immagine e l’utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
+// **BONUS 2:**
+// Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato.
+// Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
+
+
+const images = [
+  '01.jpg',
+  '02.jpg',
+  '03.jpg',
+  '04.jpg',
+  '05.jpg',
+];
+
+let imagesTags = '';
+let counterImages = 0;
+
+const slider = document.querySelector('.items-wrapper');
+const items = document.getElementsByClassName('item');
+const next = document.querySelector('.top');
+const prev = document.querySelector('.bottom');
+
+
+
+for(let i = 0; i < images.length; i++) { 
+  imagesTags += `
+    <img class="item" src="./img/${images[i]}" alt="${images[i]}">
+  `;
+ 
+}
+
+slider.innerHTML += imagesTags;
+
+items[counterImages].classList.add('active');
+
+next.addEventListener('click', function() {
+  console.log('ciao');
+})
