d2bc64667db95cadcfb5e77af6d3572cd75e649c
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-17 16:30:19 -0500

    working on bonus2

--- Bonus 2/js/script.js
+++ Bonus 2/js/script.js
@@ -1,8 +1,6 @@
 // **MILESTONE 3**
 // Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 
-
-
 const images = [
   '01.jpg',
   '02.jpg',
@@ -15,6 +13,7 @@ let imagesTags = '';
 let counterImages = 0;
 
 const slider = document.querySelector('.items-wrapper');
+const sliderThumb = document.querySelector('.items-wrap-small');
 const items = document.getElementsByClassName('item');
 const next = document.querySelector('.top');
 const prev = document.querySelector('.bottom');
@@ -29,6 +28,9 @@ for(let i = 0; i < images.length; i++) {
 }
 
 slider.innerHTML += imagesTags;
+sliderThumb.innerHTML += imagesTags;
+
+
 
 
 items[counterImages].classList.add('active');
