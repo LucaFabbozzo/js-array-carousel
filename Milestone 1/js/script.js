4bcb64557178bc0e303c077b774fc2be8deaa3c6
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-17 07:51:11 -0500

    [add] milestone 1

--- Milestone 1/js/script.js
+++ Milestone 1/js/script.js
@@ -1,1 +1,5 @@
-console.log('ciao')
+
+// **MILESTONE 1**
+// Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un’immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull’aspetto logico.
+
+
