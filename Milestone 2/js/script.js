8a4f157d5521127a89ca5e28baddce776e8f2983
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-17 09:38:32 -0500

    [add] milestone 2

--- Milestone 2/js/script.js
+++ Milestone 2/js/script.js
@@ -3,7 +3,7 @@
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
 
-const image = [
+const images = [
   '01.jpg',
   '02.jpg',
   '03.jpg',
@@ -11,3 +11,20 @@ const image = [
   '05.jpg',
 ];
 
+let imagesTags = '';
+let counterImages = 0;
+
+const slider = document.querySelector('.slider');
+const items = document.getElementsByClassName('item');
+
+for(let i = 0; i < images.length; i++) { 
+  imagesTags += `
+    <img class="item" src="./img/${images[i]}" alt="${images[i]}">
+  `;
+ 
+}
+
+
+slider.innerHTML += imagesTags;
+
+items[counterImages].classList.add('active');
